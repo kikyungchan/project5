d6dd9432fdef5f18f368c08f5eb7c32a511a7fb6
kikyungchan <kikyungchan@gmail.com>
2025-08-27 15:10:13 +0900

    회원가입 화면만 + 전역css

--- frontend/src/MemberAdd.jsx
+++ frontend/src/MemberAdd.jsx
@@ -1,4 +1,5 @@
 import {
+  Button,
   Col,
   FormCheck,
   FormControl,
@@ -7,17 +8,50 @@ import {
   Row,
 } from "react-bootstrap";
 import { use, useState } from "react";
+import { useNavigate } from "react-router";
 
 export function MemberAdd() {
+  const [id, setId] = useState("");
+  const [password1, setPassword1] = useState("");
+  const [password2, setPassword2] = useState("");
   const [name, setName] = useState("");
   const [male, setMale] = useState("");
   const [female, setFemale] = useState("");
   const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
+  const navigate = useNavigate();
+
   return (
-    <Row>
-      <Col xs={12} md={8} lg={6}>
-        <h2>회원 가입</h2>
+    <Row className="justify-content-center">
+      <Col xs={12} md={8} lg={6} xl={3}>
+        <h2 className="mb-4">회원 가입</h2>
+        <div>
+          <FormGroup>
+            <FormLabel>아이디</FormLabel>
+            <FormControl
+              value={id}
+              onChange={(e) => setId(e.target.value.trim())}
+            ></FormControl>
+          </FormGroup>
+        </div>
+        <div>
+          <FormGroup>
+            <FormLabel>비밀번호</FormLabel>
+            <FormControl
+              value={password1}
+              onChange={(e) => setPassword1(e.target.value.trim())}
+            ></FormControl>
+          </FormGroup>
+        </div>
+        <div>
+          <FormGroup>
+            <FormLabel>비밀번호 확인</FormLabel>
+            <FormControl
+              value={password2}
+              onChange={(e) => setPassword2(e.target.value.trim())}
+            ></FormControl>
+          </FormGroup>
+        </div>
         <div>
           <FormGroup>
             <FormLabel>이름</FormLabel>
@@ -26,6 +60,8 @@ export function MemberAdd() {
               onChange={(e) => setName(e.target.value.trim())}
             ></FormControl>
           </FormGroup>
+        </div>
+        <div>
           <FormGroup>
             <FormLabel>전화번호</FormLabel>
             <FormControl
@@ -33,6 +69,8 @@ export function MemberAdd() {
               onChange={(e) => setPhone(e.target.value.trim())}
             ></FormControl>
           </FormGroup>
+        </div>
+        <div>
           <FormGroup>
             <FormLabel>이메일</FormLabel>
             <FormControl
@@ -40,6 +78,8 @@ export function MemberAdd() {
               onChange={(e) => setEmail(e.target.value.trim())}
             ></FormControl>
           </FormGroup>
+        </div>
+        <div>
           <FormGroup>
             <FormCheck
               label="남"
@@ -49,6 +89,8 @@ export function MemberAdd() {
               reverse
             ></FormCheck>
           </FormGroup>
+        </div>
+        <div>
           <FormGroup>
             <FormCheck
               label="여"
@@ -58,6 +100,12 @@ export function MemberAdd() {
               reverse
             ></FormCheck>
           </FormGroup>
+        </div>
+        <div>
+          <Button>가입</Button>
+          <Button className="btn btn-secondary" onClick={() => navigate(-1)}>
+            취소
+          </Button>
         </div>
       </Col>
     </Row>
