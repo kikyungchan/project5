0941c798c87599ad61fb84353b9af55ca416a8a8
kikyungchan <kikyungchan@gmail.com>
2025-08-28 12:15:30 +0900

    진료안내 드롭다운 구상중

--- frontend/src/feature/common/Header.jsx
+++ frontend/src/feature/common/Header.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./css/Header.css";
 
 function Header() {
+  const [openMenu, setOpenMenu] = useState(null);
+
+  const toggleMenu = (menu) => {
+    setOpenMenu(openMenu === menu ? null : menu);
+  };
   return (
-    <header className="header container.nav">
+    <header className="header">
       <div className="header-top">
         <a href="/Home" className="logo">
           병원 로고
@@ -15,8 +20,22 @@ function Header() {
       </div>
       <nav className="header-nav">
         <ul className="header-menu">
-          <li>
-            <a href="/guide">진료안내</a>
+          <li
+            className="menu-item"
+            onMouseEnter={() => toggleMenu("guide")}
+            onMouseLeave={() => toggleMenu(null)}
+          >
+            진료안내
+            {openMenu === "guide" && (
+              <div className="dropdown">
+                <ul>
+                  <li>
+                    <a href="/reservation">인터넷 진료예약</a>
+                  </li>
+                  {/*  나중에 늘릴때 여기에 추가*/}
+                </ul>
+              </div>
+            )}
           </li>
           <li>
             <a href="/usage">이용안내</a>
